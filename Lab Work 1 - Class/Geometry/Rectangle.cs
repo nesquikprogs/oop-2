3189d13be2c9374ad7eca2dc3a61832edf4e36fb
nesquikprogs <nevsquik.2301@mail.ru>
2026-01-29 16:25:34 +0900

    added base structure of class

---
+++ Lab Work 1 - Class/Geometry/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GeometricFigureApp
+{
+
+    // Класс Геометрическая фигура (прямоугольник)
+    // Поддерживает задание сторон, координат на плоскости
+    // Вычисление площади и периметра
+    public class GeometricFigure
+    {
+        // Приватные поля для инкапсуляции
+        private double _sideA; // Длина стороны A
+        private double _sideB; // Длина стороны B
+        private double _x;     // Координата X 
+        private double _y;     // Координата Y
+
+        // Конструктор с параметрами.
+        public GeometricFigure(double sideA, double sideB, double x, double y)
+        {
+            if (sideA <= 0 || sideB <= 0)
+                throw new ArgumentException("Стороны должны быть положительными числами.");
+
+            _sideA = sideA;
+            _sideB = sideB;
+            _x = x;
+            _y = y;
+        }
+
+
+
+    }
+}
