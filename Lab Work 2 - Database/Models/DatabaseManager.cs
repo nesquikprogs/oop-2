cc2f3b0e4867f90a9ec10e65e4c9fa996d6cc81b
nesquikprogs <nevsquik.2301@mail.ru>
2026-02-03 19:02:35 +0900

    added model DatabaseManager

---
+++ Lab Work 2 - Database/Models/DatabaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite; // Для работы с SQLite
+using Dapper; // Для простых запросов
+
+namespace DatabaseLab.Models
+{
+    public class DatabaseManager
+    {
+        private string connectionString = "Data Source=students.db;Version=3;"; // Путь к файлу БД
+
+    }
+}
