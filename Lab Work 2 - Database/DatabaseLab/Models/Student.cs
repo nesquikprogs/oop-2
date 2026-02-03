6c9059a14538aaf607af9c07f3af41efb59f1d54
nesquikprogs <nevsquik.2301@mail.ru>
2026-02-03 18:47:05 +0900

    init commit lab + created model Student

---
+++ Lab Work 2 - Database/DatabaseLab/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace DatabaseLab.Models
+{
+    public class Student
+    {
+        public int ID { get; set; } // Уникальный идентификатор, автоинкремент в БД
+        public string Name { get; set; } // Имя студента, строка
+        public int Age { get; set; } // Возраст, int число
+        public double Grade { get; set; } // Средняя оценка, double число
+        public string Email { get; set; } // Email, строка
+    }
+}
