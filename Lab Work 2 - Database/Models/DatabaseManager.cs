a98019d9b205d1f461fe3acba164b647574a2c90
nesquikprogs <nevsquik.2301@mail.ru>
2026-02-03 19:04:00 +0900

    DatabaseManager:CreateDatabase

--- Lab Work 2 - Database/Models/DatabaseManager.cs
+++ Lab Work 2 - Database/Models/DatabaseManager.cs
@@ -9,5 +9,27 @@ namespace DatabaseLab.Models
     {
         private string connectionString = "Data Source=students.db;Version=3;"; // Путь к файлу БД
 
+        public DatabaseManager()
+        {
+            CreateDatabase(); 
+        }
+
+        private void CreateDatabase()
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Students (
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL,
+                        Age INTEGER,
+                        Grade REAL,
+                        Email TEXT
+                    );";
+                connection.Execute(createTableQuery);
+            }
+        }
+
     }
 }
