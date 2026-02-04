6dd06bcdaf37f01f1559ee1a7ce70e2461d2f5f8
nesquikprogs <nevsquik.2301@mail.ru>
2026-02-04 11:26:50 +0900

    added dockcoms in View

--- Lab Work 2 - Database/DatabaseLab/MainWindow.xaml.cs
+++ Lab Work 2 - Database/DatabaseLab/MainWindow.xaml.cs
@@ -5,53 +5,111 @@ using System.Windows.Controls;
 
 namespace DatabaseLab
 {
+    /// <summary>
+    /// Класс главного окна приложения. В MVVM представляет View.
+    /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly MainViewModel _vm;
 
+        #region Поля
+
+        /// <summary>
+        /// Поле для хранения экземпляра ViewModel главного окна.
+        /// </summary>
+        private readonly MainViewModel _vm; // Связь с ViewModel 
+
+        #endregion
+
+        #region Конструктор
+
+        /// <summary>
+        /// Конструктор главного окна.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            _vm = new MainViewModel();
-            DataContext = _vm;
+            _vm = new MainViewModel(); // Инициализация ViewModel
+            DataContext = _vm; // Установка контекста для Binding
         }
 
+        #endregion
+
+        #region Обработчики событий
+
+        /// <summary>
+        /// Обработчик события клика по кнопке "Add" для добавления нового студента.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             _vm.AddStudent();
         }
 
+        /// <summary>
+        /// Обработчик события клика по кнопке "Update" для обновления информации о студенте.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void Update_Click(object sender, RoutedEventArgs e)
         {
             _vm.UpdateStudent();
         }
 
+        /// <summary>
+        /// Обработчик события клика по кнопке "Delete" для удаления студента.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            _vm.DeleteStudent(dgStudents.SelectedItem as Student);
+            _vm.DeleteStudent(dgStudents.SelectedItem as Student); // Удаление выбранного студента с приведением типа
         }
 
+        /// <summary>
+        /// Обработчик события клика по кнопке "Search" для поиска студентов по имени.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void Search_Click(object sender, RoutedEventArgs e)
         {
             _vm.Search();
         }
 
+        /// <summary>
+        /// Обработчик события клика по кнопке "Sort" для сортировки студентов по возрасту.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void Sort_Click(object sender, RoutedEventArgs e)
         {
             _vm.SortByAge();
         }
 
+        /// <summary>
+        /// Обработчик события клика по кнопке "Refresh" для обновления списка студентов.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void Refresh_Click(object sender, RoutedEventArgs e)
         {
             _vm.Refresh();
         }
 
+        /// <summary>
+        /// Обработчик события изменения выбора в DataGrid студентов.
+        /// </summary>
+        /// <param name="sender">То, что вызывает событие (сама кнопка).</param>
+        /// <param name="e">Дополнительные аргументы события.</param>
         private void dgStudents_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (dgStudents.SelectedItem is Student selected)
+            if (dgStudents.SelectedItem is Student selected) // Создание выбранного студента с приведением типа
             {
-                _vm.CurrentStudent = selected;
+                _vm.CurrentStudent = selected; // Установка текущего студента в ViewModel
             }
         }
+
+        #endregion
+
     }
 }
